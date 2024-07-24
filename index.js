13911fdf3bfd001b14c26b0f3d171bbaadd56d85
Tiago rosa da costa <tiagorosadacost@gmail.com>
2024-07-24 16:32:40 -0300

    feat: add function to calculate the fibonacci

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ const subtract = (value1, value2) => {
     return value1 - value2
 }
 
+function fibonacci(n) {
+    if (n <= 1) {
+        return n;
+    }
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
 module.exports = {
     sum,
-    subtract
+    subtract,
+    fibonacci
 }
