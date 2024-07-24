e0cdfcfcd4da614d83a90cd53a17c403684a24d1
Tiago rosa da costa <tiagorosadacost@gmail.com>
2024-07-24 16:19:34 -0300

    feat: add function called subtract to do subtraction math

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const sum = (value1, value2) => {
     return value1 + value2
 }
 
+const subtract = (value1, value2) => {
+    return value1 - value2
+}
+
 module.exports = {
-    sum
+    sum,
+    subtract
 }
