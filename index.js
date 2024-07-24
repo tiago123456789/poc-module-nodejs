b931e64ee739d1db6d8d60f61df84d4e27690297
Tiago rosa da costa <tiagorosadacost@gmail.com>
2024-07-24 16:38:38 -0300

    refactor: changes where I putted the functions sum, subtract and fibonacci inside the MathOperation class

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const sum = (value1, value2) => {
-    return value1 + value2
-}
-
-const subtract = (value1, value2) => {
-    return value1 - value2
-}
 
 
-
-function fibonacci(n) {
-    if (n <= 1) {
-        return n;
+class MathOperation {
+    sum(value1, value2) {
+        return value1 + value2
     }
-    return fibonacci(n - 1) + fibonacci(n - 2);
+
+    subtract(value1, value2) {
+        return value1 - value2
+    }
+
+    fibonacci(n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 }
 
 module.exports = {
-    sum,
-    subtract,
-    fibonacci
+    MathOperation
 }
