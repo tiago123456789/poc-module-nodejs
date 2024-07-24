4254ff3f987caec224604e43ddea67c49a12d8c1
Tiago rosa da costa <tiagorosadacost@gmail.com>
2024-07-24 17:09:14 -0300

    docs: add comments all method to simplify how will be use the code

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 class MathOperation {
 
+    /**
+     * 
+     * @param {number} value1 
+     * @param {number} value2 
+     * @returns {number}
+     */
     sum(value1, value2) {
         return value1 + value2
     }
 
+    /**
+     * 
+     * @param {number} value1 
+     * @param {number} value2 
+     * @returns {number}
+     */
     subtract(value1, value2) {
         return value1 - value2
     }
 
+    /**
+     * @param {number} value 
+     * @param {number} valueToDivide 
+     * @returns {number}
+     */
     divideBy(value, valueToDivide) {
         return value / valueToDivide
     }
 
+    /**
+     * 
+     * @param {number} n 
+     * @returns {number}
+     */
     fibonacci(n) {
         if (n <= 1) {
             return n;
